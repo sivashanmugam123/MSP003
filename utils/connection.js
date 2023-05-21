16035660feee0859069faa6c3ce0ae9cea9ab0cb
sivashanmugam123 <malthesiva@gmail.com>
2023-05-21 08:35:30 +0530

    7th commit

--- utils/connection.js
+++ utils/connection.js
@@ -5,7 +5,9 @@ var db = mysql.createPool({
     host:"sql1233.mysql.database.azure.com",
     user:"nethrasiva",
     password:"Welcome123!",
-    database:"cutoff"
+    database:"cutoff",
+    port: 3306,
+    ssl: {ca: fs.readFileSync("./DigiCertGlobalRootCA.crt.pem")}
 });
 // db.connect(
 //     function (err) { 
